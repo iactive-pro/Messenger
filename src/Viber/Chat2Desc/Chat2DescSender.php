0c2cf33d45347a7e1056b4832d2ad8dcf743b659
Aleksandr Yasakov <yasakov.org@gmail.com>
2020-10-30 15:44:56 +0300

    chat api caption

--- src/Viber/Chat2Desc/Chat2DescSender.php
+++ src/Viber/Chat2Desc/Chat2DescSender.php
@@ -7,9 +7,9 @@ use src\Whatsapp\Chat2Desc\Chat2DescSender as Sender;
 
 class Chat2DescSender extends Sender
 {
-    public function __construct(string $token)
+    public function __construct(string $token, $channelId)
     {
-        parent::__construct($token);
+        parent::__construct($token, $channelId);
     }
 
     protected const TRANSPORT = BaseServiceRequest::TRANSPORT_VIBER;
